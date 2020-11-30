5e90844d22dd2ecefc071e372176509d005dafcd
Samuel Bishop <sjbc35@mail.missouri.edu>
2020-11-30 17:34:39 -0600

    Initial push to get this project up on its feet

---
+++ AVSS_Main.py
@@ -0,0 +1,14 @@
+"""
+This project is going to be awesome. Need to research I/O in python for the jetson nano,
+also need to research tensorflow lite
+"""
+
+# The function that does all of the image processing
+def magic(img):
+    finished = 1
+    # Process this image, set the relevant pin outputs, return finished
+    return imgFromVideoFeed
+
+if __name__ == '__main__':
+    imgFromVideoFeed = "I don't know at all what im doing"
+    magic(imgFromVideoFeed)
